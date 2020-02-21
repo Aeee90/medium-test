94e9a59b6c0da4246f5173cc2fe4598933f1b38b
Lee Seoyoeon <sps1122@mutecsoft.com>
2020-02-21 10:17:59 +0900

    init

---
+++ src/components/Editor.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="editor" >
+
+  </div>
+</template>
+<script>
+    import MediumEditor from 'medium-editor';
+
+    const editor = new MediumEditor('#editor', {});
+
+    export default {
+        name: "Editor"
+    }
+</script>
+
+<style scoped>
+
+</style>
